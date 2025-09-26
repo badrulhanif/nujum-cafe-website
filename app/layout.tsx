2c2348bb4b379f472ce77780f84d9e8e83eec132
Badrul Hanif <hanifbaharuddin@gmail.com>
2025-09-26 12:04:41 +0800

    update google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,15 +87,15 @@ export default function RootLayout({
       <head>
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-K04WZGQE7M"
+          src="https://www.googletagmanager.com/gtag/js?id=G-KFM2K7B5L6"
         />
         <Script id="google-analytics">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-K04WZGQE7M');
-          `}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KFM2K7B5L6');
+        `}
         </Script>
         <Script
           id="ldjson-cafe"
